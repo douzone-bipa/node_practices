b6dd0f033b63fda13c29f5d7f0438f8dedfeb9c1
douzone-bipa <douzone.bipa@gmail.com>
2021-11-16 14:49:00 +0900

    emaillist #1

--- helloweb-ex02/routes/user.js
+++ helloweb-ex02/routes/user.js
@@ -20,9 +20,24 @@ router.route("/join").get(function(req, res){
     res.render('user/join');
 });
 
-router.route("/join").post(function(req, res){
+router.route("/join").post(function(req, res) {
     console.log(req.body);
     res.redirect("/");
+});
+
+router.route("/api").get(function(req, res){
+    const vo = {
+        no: 10,
+        name: '둘리',
+        email: 'dooly@gmail.com',
+        gender: 'male'
+    };
+
+    // res.writeHead(200, {
+    //     'Content-Type': "application/json"
+    // });
+    // res.end(JSON.stringify(vo));
+    res.send(vo);
 });
 
 module.exports = router;
