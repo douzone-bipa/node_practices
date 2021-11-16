ee93dbdb9185bcae82f316c0d0404bff8aec24bd
douzone-bipa <douzone.bipa@gmail.com>
2021-11-16 17:12:17 +0900

    emaillist #final

--- emaillist/controllers/emaillist.js
+++ emaillist/controllers/emaillist.js
@@ -3,10 +3,17 @@ const model = require('../models/emaillist');
 
 module.exports = {
     index: async function(req, res) {
-        const results = await model.findAll(function(){});
+        const results = await model.findAll();
         res.render('index', {
             list: results || []
         });
+    },
+    form: function(req, res) {
+        res.render('form');
+    },
+    add: async function(req, res) {
+        const results = await model.insert(req.body);
+        res.redirect("/");
     }
 }
 
