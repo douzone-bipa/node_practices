f3701346552b2ca2d538156b59941b052f04f3d1
douzone-bipa <douzone.bipa@gmail.com>
2021-11-19 12:22:08 +0900

    gallery #finish

--- mysite/routes/gallery.js
+++ mysite/routes/gallery.js
@@ -3,5 +3,7 @@ const controller = require('../controllers/gallery');
 
 const router = express.Router();
 router.route('').get(controller.index);
+router.route('/upload').post(controller.upload);
+
 
 module.exports = router
